e2c4c080877274b5019211f9ee18284ef53b2e52
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-10-09 10:41:16 +0000

    fix: init script

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -16,6 +16,7 @@ docker run -v $REPO_ROOT/ocm-stub:/ocm-stub stub npm install
 docker run -v $REPO_ROOT/reva:/reva \
   -e"PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
   --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make reva && make revad"
+
 docker run -v $REPO_ROOT/nc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh nc1 make composer
 docker run -v $REPO_ROOT/oc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh oc1 make composer
 
